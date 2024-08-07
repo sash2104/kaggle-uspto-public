5cb79aaf393b2bf40f145e12a8081a335929defc
sash <sash2104@gmail.com>
2024-08-07 06:34:45 +0000

    update solver

--- solve.sh
+++ solve.sh
@@ -2,8 +2,7 @@
 
 outdir=$(cat SETTINGS.json | jq -r .OUTDIR)
 g++ -std=c++17 -O3 src/main.cpp -o src/run
-# src/run --datadir "${outdir}/" --field cpc --field title --field abstract --field claims --field description > ${outdir}/result.txt
-src/run --datadir "${outdir}/" --field cpc --field title --field abstract --field claims > ${outdir}/result.txt
+src/run --datadir "${outdir}/" --field cpc --field title --field abstract --field claims --field description > ${outdir}/result.txt
 
 python script/submit.py -f SETTINGS.json
 
